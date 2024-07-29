a32df8ae65b523e09aa6d963f86cd60fc3c63332
ClaytonSantos1 <58684030+ClaytonSantos1@users.noreply.github.com>
2024-07-29 18:10:49 -0300

    Hipp Hipp

---
+++ hipphipp.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Kattis{
+    public static void Main(String[] args){
+        for(int i=0;i < 20; i++){
+            Console.WriteLine("Hipp hipp hurra!");
+        }
+    }
+}
