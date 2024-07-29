e2a2041b848e1787daa13bae5f32373ae0eabd05
ClaytonSantos1 <58684030+ClaytonSantos1@users.noreply.github.com>
2024-07-29 20:11:19 -0300

    Kveðja

---
+++ kvedja.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Kattis{
+    public static void Main(String[] args){
+        string name = Console.ReadLine();
+        Console.WriteLine("Kvedja,");
+        Console.WriteLine(name);
+    }
+}
