a1cd815a728c78402a86ba7f1083b2652f8992d2
ClaytonSantos1 <58684030+ClaytonSantos1@users.noreply.github.com>
2024-07-29 19:31:49 -0300

    Draga Frá

---
+++ dragafra.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Kattis{
+    public static void Main(String[] args){
+        int n = Convert.ToInt32(Console.ReadLine());
+        int m = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine(n - m);
+    }
+}
