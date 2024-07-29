b594ab81d93eb2a99078b6438f8476a4b75ff66a
ClaytonSantos1 <58684030+ClaytonSantos1@users.noreply.github.com>
2024-07-29 18:49:51 -0300

    Tölvunarfræðingar telja

---
+++ tolvunarfraedingartelja.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Kattis{
+    public static void Main(String[] args){
+        int n = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine(n - 1);
+    }
+}
