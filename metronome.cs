1c49fbbd92b18377c2b5e65ab51c75025ce3f6b3
ClaytonSantos1 <58684030+ClaytonSantos1@users.noreply.github.com>
2024-07-29 21:18:34 -0300

    Metronome

---
+++ metronome.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Kattis{
+    public static void Main(String[] args){
+        decimal n = Convert.ToDecimal(Console.ReadLine());
+        
+        Console.WriteLine(n/4);
+    }
+}
